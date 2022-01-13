00f5a1013db6a777911eba29bf6f188f14ade273
LucianoChichari <lucianochichari@gmail.com>
2022-01-13 16:23:26 -0300

    Updates

---
+++ precache-manifest.ae29cf06fc22bc32c9983ca4bcbd29b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979e65bbd7e3414149118adb846bf754",
+    "url": "/LandingSideProjectOne/index.html"
+  },
+  {
+    "revision": "d362f315c1c4491b2d1c",
+    "url": "/LandingSideProjectOne/static/css/2.44741ecb.chunk.css"
+  },
+  {
+    "revision": "26960b97752845ef5187",
+    "url": "/LandingSideProjectOne/static/css/main.fab8896c.chunk.css"
+  },
+  {
+    "revision": "d362f315c1c4491b2d1c",
+    "url": "/LandingSideProjectOne/static/js/2.b73dd311.chunk.js"
+  },
+  {
+    "revision": "26960b97752845ef5187",
+    "url": "/LandingSideProjectOne/static/js/main.e29961df.chunk.js"
+  },
+  {
+    "revision": "2b9043f3a2fc320955fd",
+    "url": "/LandingSideProjectOne/static/js/runtime~main.1a2a65c9.js"
+  },
+  {
+    "revision": "6af1284aecdc61b44d932dc3be23a5e1",
+    "url": "/LandingSideProjectOne/static/media/kv.6af1284a.png"
+  }
+]);
